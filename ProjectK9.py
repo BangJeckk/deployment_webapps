abd5bef6d2c84eddc3ce3d7b53254e981cc1fd04
BangJeckk <fauzannur2912@gmail.com>
2023-05-08 09:28:18 +0800

    ProjectK9

--- ProjectK9.py
+++ ProjectK9.py
@@ -3,10 +3,6 @@ import math
 
 st.title('Aplikasi perhitungan normalitas dan persentase kadar')
 
-default_value = 1.0000
-min_value = 0.0001
-max_value = 9999.0000
-
 st.write('Oleh Kelompok 9')
 st.write('Ali Akbar Mustaqim (2219030)')
 st.write('Jecly Nur Fauzan (2219088)')
@@ -15,55 +11,66 @@ st.write('Muhammad Dzaki Pratama Putra (2219114)')
 st.write('Rai Aria Yudistira (2219148)')
 
 
-massa = st.number_input('Masukkan nilai massa (mg)',format="%.4f",value=default_value, min_value=min_value, max_value=max_value)
-volume = st.number_input('Masukkan nilai volume',format="%.2f",value=default_value, min_value=min_value, max_value=max_value)
-BE1 = st.number_input('Masukkan nilai BE',format="%.1f",value=default_value, min_value=min_value, max_value=max_value)
-FP1 = st.number_input('Masukkan nilai F Pengali',format="%.0f",value=default_value, min_value=min_value, max_value=max_value)
+tab1, tab2, = st.tabs(["Perhitungan Normalitas dan Kadar", "Perhitungan pH",])
 
-tombol = st.button('Hitung nilai normalitasnya')
-
-nilai_normalitas1=massa/(BE1*volume*FP1)
-if tombol:
-    nilai_normalitas = massa/(BE1*volume*FP1)
-    st.success(f'Nilai normalitas adalah {nilai_normalitas}')
-
-
-Vtitran = st.number_input('Masukkan nilai volume titran (mL)')
-Ntitran = st.number_input('Masukkan nilai normalitas titran',format='%.4f', value=(nilai_normalitas1))
-BE2 = st.number_input('Masukkan nilai BEnya',format="%.1f")
-FP2 = st.number_input('Masukkan nilai F Pengalinya',format="%.0f")
-Vtitrat = st.number_input('Masukkan nilai volume titrat (mL)',format="%.0f")
-
-tombol = st.button('Hitung nilai kadarnya')
-
-if tombol:
-    nilai_kadar = (Vtitran*Ntitran*BE2*10**-3*FP2*100)/Vtitrat 
-    st.success(f'Persentase kadarnya adalah {nilai_kadar}%')
+with tab1:
     
+    st.title("Kalkulator Normalitas dan Kadar")
+    default_value = 1.0000
+    min_value = 0.0000
+    max_value = 9999.0000
     
-st.title("Kalkulator Logaritma")
+    massa = st.number_input('Masukkan nilai massa (mg)',format="%.4f",value=default_value,)
+    volume = st.number_input('Masukkan nilai volume (mL)',format="%.2f",value=default_value,)
+    BE1 = st.number_input('Masukkan nilai BE',format="%.1f",value=default_value,)
+    FP1 = st.number_input('Masukkan nilai F Pengali',format="%.0f",value=default_value,)
 
-default_value1 = 1.0000
-min_value1 = 0.0000
-max_value1 = 9999.0000
-    
-jenis_larutan = st.radio("Jenis larutan yang diukur:", ["Asam", "Basa"])
+    tombol = st.button('Hitung nilai normalitasnya')
 
-if jenis_larutan == "Asam":
-    st.write("Larutan asam: HCl, H2SO4, HNO3, dll.")
-    # Input konsentrasi ion hidrogen (H+) dalam mol/L
-    c_H = st.number_input("Masukkan konsentrasi ion H+ (mol/L):", step=0.01, format="%.4f", value=default_value, min_value=min_value, max_value=max_value)
-    # Menghitung pH larutan
-    pH = -math.log10(c_H)
-    st.write(f"pH larutan: {pH:.2f}",)
+    nilai_normalitas1=massa/(BE1*volume*FP1)
+    if tombol:
+        nilai_normalitas = massa/(BE1*volume*FP1)
+        st.success(f'Nilai normalitas adalah {nilai_normalitas}')
+
+
+    Vtitran = st.number_input('Masukkan nilai volume titran (mL)')
+    Ntitran = st.number_input('Masukkan nilai normalitas titran (N)',format='%.4f', value=(nilai_normalitas1))
+    BE2 = st.number_input('Masukkan nilai BEnya',format="%.1f")
+    FP2 = st.number_input('Masukkan nilai F Pengalinya',format="%.0f")
+    Vtitrat = st.number_input('Masukkan nilai volume titrat (mL)',format="%.0f")
+
+    tombol = st.button('Hitung nilai kadarnya')
+
+    if tombol:
+        nilai_kadar = (Vtitran*Ntitran*BE2*10**-3*FP2*100)/Vtitrat 
+        st.success(f'Persentase kadarnya adalah {nilai_kadar}%')   
+
+with tab2:
     
-elif jenis_larutan == "Basa":
-    st.write("Larutan basa: NaOH, KOH, Ca(OH)2, dll.")
-    # Input konsentrasi ion hidroksida (OH-) dalam mol/L
-    c_OH = st.number_input("Masukkan konsentrasi ion OH- (mol/L):", step=0.01, format="%.4f", value=default_value, min_value=min_value, max_value=max_value)
-    # Menghitung pOH larutan
-    pOH = -math.log10(c_OH)
-    # Menghitung pH larutan
-    pH = 14 - pOH
-    st.write(f"pH larutan: {pH:.2f}")
+    st.title("Kalkulator pH Larutan")
+    default_value = 1.0000
     
+
+    jenis_larutan = st.radio("Jenis larutan yang diukur:", ["Asam", "Basa"])
+
+    if jenis_larutan == "Asam":
+        st.write("Larutan asam: HCl, H2SO4, HNO3, dll.")
+        # Input konsentrasi ion hidrogen (H+) dalam mol/L
+        c_H = st.number_input("Masukkan konsentrasi ion H+ (mol/L):", format="%.10f", value=default_value,)
+        # Menghitung pH larutan
+        pH = -math.log10(c_H)
+        st.write(f"pH larutan: {pH:.2f}",)
+     
+    
+    elif jenis_larutan == "Basa":
+        st.write("Larutan basa: NaOH, KOH, Ca(OH)2, dll.")
+        # Input konsentrasi ion hidroksida (OH-) dalam mol/L
+        c_OH = st.number_input("Masukkan konsentrasi ion OH- (mol/L):", format="%.10f", value=default_value,)
+        # Menghitung pOH larutan
+        pOH = -math.log10(c_OH)
+        # Menghitung pH larutan
+        pH = 14 - pOH
+        st.write(f"pH larutan: {pH:.2f}")
+          
+    
+
